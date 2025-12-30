b7734c319a3eb01a41cc1cecc22b035d0ddc4df1
Vishal Patil <vishalpatilvgp2199@gmail.com>
2025-12-30 13:52:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+#btn{
+background-color:black;
+color:white;
+border:none;
+border-radius:5px;
+padding:10px 20px;
+cursor:pointer;
+}
+
+#btn:hover{
+background-color:yellow;
+color:black;
+border-radius:30px;
+}
